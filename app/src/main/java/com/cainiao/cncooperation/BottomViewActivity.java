5a11633ef29d7da3ef9e6ef337801e992d1e2c76
hc <baifaqingsi@gmail.com>
2017-07-23 17:28:08 +0800

    将主界面改用BottonNavigationview+viewpager展示

---
+++ app/src/main/java/com/cainiao/cncooperation/BottomViewActivity.java
@@ -0,0 +1,113 @@
+package com.cainiao.cncooperation;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.cainiao.cncooperation.adapter.BottomViewPagerAdapter;
+import com.cainiao.cncooperation.fragment.AttentionFragment;
+import com.cainiao.cncooperation.fragment.DiscoveryFragment;
+import com.cainiao.cncooperation.fragment.HomeFragment;
+import com.cainiao.cncooperation.fragment.ProfileFragment;
+import com.cainiao.cncooperation.fragment.ShoppingCartFragment;
+import com.cainiao.cncooperation.util.BottomNavigationViewHelper;
+
+public class BottomViewActivity extends AppCompatActivity {
+
+    public BottomNavigationView mBottomNavigationView;
+    public ViewPager mViewPager;
+    private MenuItem menuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bottom_navigation_bar);
+        initView();
+    }
+
+
+    /***
+     * 初始化主界面的布局
+     */
+    private void initView() {
+
+        mBottomNavigationView  = (BottomNavigationView) findViewById(R.id.navigation);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        BottomNavigationViewHelper.disableShiftMode(mBottomNavigationView);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.menu_home:
+                                mViewPager.setCurrentItem(0);
+                                break;
+                            case R.id.menu_discovery:
+                                mViewPager.setCurrentItem(1);
+                                break;
+                            case R.id.menu_attention:
+                                mViewPager.setCurrentItem(2);
+                                break;
+                            case R.id.menu_profile:
+                                mViewPager.setCurrentItem(3);
+                                break;
+                            case R.id.menu_shopping_cart:
+                                mViewPager.setCurrentItem(4);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (menuItem != null) {
+                    menuItem.setChecked(false);
+                } else {
+                    mBottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                menuItem = mBottomNavigationView.getMenu().getItem(position);
+                menuItem.setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+
+
+/*      //禁止viewpager的滑动事件
+        mViewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+*/
+        setupViewPager(mViewPager);
+
+    }
+
+
+    private void setupViewPager(ViewPager viewPager) {
+        BottomViewPagerAdapter adapter = new BottomViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(HomeFragment.newInstance("主页"));
+        adapter.addFragment(DiscoveryFragment.newInstance("发现"));
+        adapter.addFragment(AttentionFragment.newInstance("关注"));
+        adapter.addFragment(ProfileFragment.newInstance("我的"));
+        adapter.addFragment(ShoppingCartFragment.newInstance("购物车"));
+        viewPager.setAdapter(adapter);
+    }
+
+}
