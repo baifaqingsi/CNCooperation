aa886f77a6f23efcb9945a91e781e3c106c34ebc
hc <baifaqingsi@gmail.com>
2017-07-23 17:28:08 +0800

    将主界面改用BottonNavigationview+viewpager展示

---
+++ app/src/main/java/com/cainiao/cncooperation/adapter/BottomViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.cainiao.cncooperation.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Hc on 2017/7/23.
+ */
+
+public class BottomViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public BottomViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+}
